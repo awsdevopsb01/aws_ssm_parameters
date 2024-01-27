6dcd5e4a78187e679f2d05080fd088578507b6ad
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-27 21:20:38 +0530

    practice Ansible parameters - encryption

--- parameters.tf
+++ parameters.tf
@@ -2,5 +2,7 @@ resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
-  type = "String"
+  type = "SecureString"
+  key_id = "1598ad31-8c90-467c-8523-f3a951215606"
 }
+
